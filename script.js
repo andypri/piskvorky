38b498ea5beacfb3873e120a909767da9c9c6f3d
Andrea Příhoda <andyprihoda@gmail.com>
2021-04-11 22:30:52 +0200

    JS skoro hotovo, chybí animation

--- script.js
+++ script.js
@@ -11,14 +11,18 @@ for (let i = 0; i < tlacitka.length; i += 1) {
   tlacitko.addEventListener('click', (event) => {
     const tlacitkoPoStisknuti = event.target;
     const hracNaTahuSymbol = document.querySelector('.hrac__symbol');
-    if (hracNaTahu === 'circle') {
-      tlacitkoPoStisknuti.classList.add('button--circle');
-      hracNaTahu = 'cross';
-      hracNaTahuSymbol.src = 'cross.svg';
-    } else if (hracNaTahu === 'cross') {
-      tlacitkoPoStisknuti.classList.add('button--cross');
-      hracNaTahu = 'circle';
-      hracNaTahuSymbol.src = 'circle.svg';
+    if (tlacitkoPoStisknuti.classList.contains('empty')) {
+      if (hracNaTahu === 'circle') {
+        tlacitkoPoStisknuti.classList.replace('empty', 'button--circle');
+        tlacitkoPoStisknuti.setAttribute('disabled', true);
+        hracNaTahu = 'cross';
+        hracNaTahuSymbol.src = 'cross.svg';
+      } else if (hracNaTahu === 'cross') {
+        tlacitkoPoStisknuti.classList.replace('empty', 'button--cross');
+        tlacitkoPoStisknuti.setAttribute('disabled', true);
+        hracNaTahu = 'circle';
+        hracNaTahuSymbol.src = 'circle.svg';
+      }
     }
   });
 }
