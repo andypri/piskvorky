47b131cc62eb555598f487a5b771d949f817522f
Andrea Příhoda <andyprihoda@gmail.com>
2021-04-11 21:26:23 +0200

    Přidání symbolů v HTML, JS 5.iv a 5.v

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const hracNaTahu = 'cross';
+let hracNaTahu = 'circle';
 
 const tlacitka = document.querySelectorAll('button');
 console.log(tlacitka);
@@ -10,11 +10,15 @@ for (let i = 0; i < tlacitka.length; i += 1) {
 
   tlacitko.addEventListener('click', (event) => {
     const tlacitkoPoStisknuti = event.target;
+    const hracNaTahuSymbol = document.querySelector('.hrac__symbol');
     if (hracNaTahu === 'circle') {
       tlacitkoPoStisknuti.classList.add('button--circle');
-    }
-    if (hracNaTahu === 'cross') {
+      hracNaTahu = 'cross';
+      hracNaTahuSymbol.src = 'cross.svg';
+    } else if (hracNaTahu === 'cross') {
       tlacitkoPoStisknuti.classList.add('button--cross');
+      hracNaTahu = 'circle';
+      hracNaTahuSymbol.src = 'circle.svg';
     }
   });
 }
